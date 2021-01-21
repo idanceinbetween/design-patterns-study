64d90465b7747328bfa2c42104f677df738acac7
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2021-01-21 22:40:33 +0000

    Improves Factory pattern with more details

--- src/main/kotlin/factory/AnimalFactory.kt
+++ src/main/kotlin/factory/AnimalFactory.kt
@@ -1,65 +1,74 @@
 package factory
 
 fun runFactory(option: String) {
-    // val makeAnimal: Animal = AnimalFactory().makeAnimal() //cannot create an instance of an abstract class [1]
+    println("Client code asked for a [$option] to be created")
+    println("Try and change this to Tiger or Duck and see the differences")
+    // val makeAnimal: Animal = AnimalFactory().makeAnimal() //cannot create an instance of an abstract class
     when (option) {
         "dog" -> {
-            val dogFactoryOutput = DogFactory()
-            dogFactoryOutput.oozeCuteness()
-            dogFactoryOutput.makeAnimal() // makeAnimal is a function in abstractClass AnimalFactory, not DogFactory
+            println("Calling Dog Factory")
+            val dogFactory = DogFactory()
+            dogFactory.orchestrateCreation() // orchestrateCreation is a function in abstract class AnimalFactory, not DogFactory
+            dogFactory.saleOn() // DogFactory extends abstract class AnimalFactory and implements Shop
         }
         "tiger" -> {
-            TigerFactory().makeAnimal()
-            // TigerFactory().oozeCuteness() // TigerFactory did not implement BabyAnimals!
+            TigerFactory().orchestrateCreation()
+            // TigerFactory().saleOn() // TigerFactory did not implement Shop!
         }
-        "duck" -> DuckFactory().makeAnimal()
-        else -> throw RuntimeException("Pick a dog or tiger!")
+        "duck" -> DuckFactory().orchestrateCreation()
+        else -> throw RuntimeException("Pick a dog, tiger or duck!")
     }
 }
 
+// here the AnimalFactory is an abstract class
+// however it does not have to be strictly the case.
+// There could be a default factory method if the
+// creator has no subclasses, in which case it cannot be
+// an abstract class.
 abstract class AnimalFactory {
-    fun makeAnimal(): Animal { // Animal is an interface
-        println("Creating an animal in an Animal Factory")
+    fun orchestrateCreation(): Animal { // Animal is an interface
+        println("Orchestrating creation in Animal Factory following a client request received by a subclass Factory")
         val animal = createAnimal() // deferring instantiation to a subclass
-        println("Animal Factory asking created animal - the 'product' - to speak and act")
+        println("Animal Factory asking created animal (the 'product') to speak and act")
         animal.speak() // this method is overridden by the class that extends Animal
         animal.preferredAction()
         // hunt() doesn't work because it is a method of an interface that only the Tiger
         // (an extension of the Animal abstract class) implements. Dog, even though extends an Animal,
         // does not implement this interface.
-        // animal.hunt()
+//         animal.hunt()
         return animal
     }
 
     abstract fun createAnimal(): Animal // an unimplemented method that MUST be overridden by the class extended AnimalFactory
 }
 
-interface BabyAnimals {
-    fun oozeCuteness() {
-        println("baby animals are cute")
+interface Shop {
+    fun saleOn() {
+        println("By the way, this subclass Factory implemented a Shop interface, and there's a sale!")
     }
 }
 
-// DogFactory extends abstract class AnimalFactory and implements BabyAnimals
-class DogFactory : AnimalFactory(), BabyAnimals {
+// DogFactory extends abstract class AnimalFactory and implements Shop
+class DogFactory : AnimalFactory(), Shop {
     override fun createAnimal(): Dog {
-        println("subclass DuckFactory instantiating")
+        println("subclass DogFactory instantiated and created a Dog with createAnimal")
         return Dog()
     }
 }
 
 class TigerFactory : AnimalFactory() {
     override fun createAnimal(): Tiger {
-        println("subclass TigerFactory instantiating")
+        println("subclass TigerFactory instantiated and created a Tiger with createAnimal")
         return Tiger()
     }
 }
 
 class DuckFactory : AnimalFactory() {
     override fun createAnimal(): Duck {
-        println("subclass DuckFactory instantiating")
+        println("subclass DuckFactory instantiated and created a Duck with createAnimal")
         val duck = Duck()
-        duck.swims() // this is an implemented method
+        println("By the way the Duck is also a Bird")
+        duck.swims() // this is an implemented method in the interface Bird that Duck implements
         return duck
     }
 }
