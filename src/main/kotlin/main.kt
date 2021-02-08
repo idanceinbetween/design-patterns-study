a0ba70a155cb31525e9a5e720bcb126a346fdfd3
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2021-02-08 22:11:16 +0000

    Calls strategy pattern code from main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,22 @@
-import factory.runFactory
-import factoryAgain.orderPizzaFromAPizzaStoreFranchise
+import strategy.FlyWithWings
+import strategy.MallardDuck
+import strategy.RubberDuck
 
 fun main() {
 //    runSingleton()
 //    runPrototype()
 //    runFactory("dog")
-    orderPizzaFromAPizzaStoreFranchise("cheese", "Italy")
-    orderPizzaFromAPizzaStoreFranchise("pepperoni", "New York")
+//    orderPizzaFromAPizzaStoreFranchise("cheese", "Italy")
+//    orderPizzaFromAPizzaStoreFranchise("pepperoni", "New York")
+
+    val mallard = MallardDuck()
+    mallard.display()
+    mallard.performQuack()
+    mallard.performFly()
+
+    val rubber = RubberDuck()
+    rubber.display()
+    rubber.performFly()
+    rubber.setFly(FlyWithWings())
+    rubber.performFly()
 }
